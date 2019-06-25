28944927f73de52c5cacc5f7198e1ddcc8e59fa4
Valdemar Pereira <valdemar.developer@gmail.com>
2019-06-25 19:48:39 +0100

    Spring Boot bootstrap

--- src/test/java/com/valdemar/codenamesduet/CodeNamesDuetApplicationTest.java
+++ src/test/java/com/valdemar/codenamesduet/CodeNamesDuetApplicationTest.java
@@ -8,8 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
-class CodeNamesDuetApplicationTest {
+public class CodeNamesDuetApplicationTest {
 
     @Test
     public void contextLoads() {
